f4ed5d38c846531fd5437c90b5728b4f0caf88a4
uchihahaha-huhu <tamb2014696@student.ctu.edu.vn>
2024-11-15 10:47:02 +0700

    Update changes in flutter submodule

--- dev/benchmarks/multiple_flutters/module/lib/main.dart
+++ dev/benchmarks/multiple_flutters/module/lib/main.dart
@@ -1,7 +1,3 @@
-// Copyright 2014 The Flutter Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
 import 'package:flutter/material.dart';
 import 'package:flutter/semantics.dart';
 import 'package:google_fonts/google_fonts.dart';
